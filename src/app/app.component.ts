73aa81cfe77a74b9ad4985ce6bb23f468d27a89f
Gabriel Vargas <darklouses@gmail.com>
2023-09-26 22:11:08 +0200

    Co-authored-by: Gabriel Vargas <darklouses@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './models/product.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
+
 export class AppComponent {
-  title = 'angular';
+  title:string = 'gabriel';
+  Http = inject(HttpClient);
+  ProductList: Product[] = [];
+
+  changeTitle():void {
+    this.title = 'changed';
+  }
+
+  ngOnInit() {
+    this.Http.get<Product[]>('https://api.escuelajs.co/api/v1/products')
+      .subscribe((data) => {
+        this.ProductList = data;
+      }); 
+  }
 }
+
+
